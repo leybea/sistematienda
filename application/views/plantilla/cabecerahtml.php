4918cf2375abc32f9aadeb0103591e33c740f157
Brenda Lily Gutierrez Gutierrez <brenly2209@gmail.com>
2019-02-05 16:00:21 -0400

    titulo de pagina cambiada

--- application/views/plantilla/cabecerahtml.php
+++ application/views/plantilla/cabecerahtml.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Sistema tienda Sinai</title>
+    <title>Sistema tienda</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="<?=base_url();?>bootstrap/css/bootstrap.min.css" rel="stylesheet">
